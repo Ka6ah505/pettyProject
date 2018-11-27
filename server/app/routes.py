0890ca3bbedd51886f46813599b2be424098cb9a
Sergey Mironov <ka6ah505@gmail.com>
2018-11-27 23:05:31 +0300

    добавлен шаблонизатор стартовой страницы

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,17 +1,9 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     # user = jsonify({'author': 'Sergey Mironov'})
     user = {'username': 'Sergey Mironov'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+    return render_template('index.html', title='Test App', user=user)
