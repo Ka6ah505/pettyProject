8a1fb255c62bc86456576f90482fe8ff150bd884
Sergey Mironov <ka6ah505@gmail.com>
2018-11-29 17:22:05 +0300

    Добавил форму авторизации пользователя

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import jsonify, render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +9,8 @@ def index():
     # user = jsonify({'author': 'Sergey Mironov'})
     user = {'username': 'Sergey Mironov'}
     return render_template('index.html', title='Test App', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
