65640d2acedcb799b0899dfdebe02a8f6e695065
Sergey Mironov <ka6ah505@gmail.com>
2018-11-29 17:54:28 +0300

    Добавил простую логику для обработки формы регистрации

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify, render_template
+from flask import jsonify, render_template,flash, redirect
 from app.forms import LoginForm
 
 
@@ -10,7 +10,13 @@ def index():
     user = {'username': 'Sergey Mironov'}
     return render_template('index.html', title='Test App', user=user)
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember_me {}'. fromat(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
