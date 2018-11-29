8f7a566ae109a9ca47311cbbebb8d431061b8873
Sergey Mironov <ka6ah505@gmail.com>
2018-11-29 19:49:52 +0300

    Добавил обработчик ошибок при заполнении формы аутентификации

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify, render_template,flash, redirect
+from flask import jsonify, render_template,flash, redirect, url_for
 from app.forms import LoginForm
 
 
@@ -11,12 +11,12 @@ def index():
     return render_template('index.html', title='Test App', user=user)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me {}'. fromat(
+        flash('Login request for user {}, remember_me {}'. format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
