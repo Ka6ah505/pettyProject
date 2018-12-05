01044f768b3cdb213bccf73a5e0b785bbc419f36
Sergey Mironov <ka6ah505@gmail.com>
2018-12-05 13:28:54 +0300

    добавлен декоратор, который регистрирует объекты в сеансе приложения

--- server/server.py
+++ server/server.py
@@ -1,3 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Project, Task
 
-app.run(host='0.0.0.0')
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Project': Project, 'Task': Task}
+
+#app.run(host='0.0.0.0')
