ca2b7f99f217c51fe3b5b8ca55d0a6b51d999e0b
Sergey Mironov <ka6ah505@gmail.com>
2018-12-28 13:46:40 +0300

    Добавил ответ на запрос от клиента

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'author': 'Sergey Mironov'})
+    data = {
+        'id': 'client',
+        'author': 'Sergey Mironov'
+    }
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 app.run(host='0.0.0.0')
